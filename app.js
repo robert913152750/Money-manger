b41bd4fa05fc3071d3c55f626c18b82078c573b1
Robert <robert913152750@gmail.com>
2020-05-05 19:49:49 +0800

    Add mongoose connection

--- app.js
+++ app.js
@@ -1,8 +1,27 @@
 //require express and some base setting
 const express = require("express");
 const app = express();
+const mongoose = require("mongoose");
 
-//use routes
+//setting mongoDB
+mongoose.connect("mongodb://localhost/expense", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+const db = mongoose.connection; //連線後拿到connection 物件
+
+//db 連線異常
+db.on("error", () => {
+  console.log("mongodb error!");
+});
+
+//db 連線成功
+db.once("open", () => {
+  console.log("mongodb connected!");
+});
+
+//load routes
+app.use("/", require("./routes/home"));
 
 //listen express
 app.listen(3000, () => {
