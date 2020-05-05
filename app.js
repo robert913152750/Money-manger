650ce6b13f0ab08106a00ad17c09ae0c021bf7ea
Robert <robert913152750@gmail.com>
2020-05-05 17:41:33 +0800

    Initialize project

---
+++ app.js
@@ -0,0 +1,10 @@
+//require express and some base setting
+const express = require("express");
+const app = express();
+
+//use routes
+
+//listen express
+app.listen(3000, () => {
+  console.log("App is running");
+});
