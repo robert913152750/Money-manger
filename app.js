920fa7dceb40e22f76f68190457ca62a04406413
Robert <robert913152750@gmail.com>
2020-05-11 18:33:57 +0800

    Install handlebars and set index template

--- app.js
+++ app.js
@@ -3,6 +3,13 @@ const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 
+//require express-handlebars
+const exphbs = require("express-handlebars");
+
+//tell express use handlebars to be template engine and set "main" for defaulted layout
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
 //setting mongoDB
 mongoose.connect("mongodb://localhost/expense", {
   useNewUrlParser: true,
--- routes/home.js
+++ routes/home.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 
 router.get("/", (req, res) => {
-  res.send("This app will be expense-tracker");
+  res.render("index");
 });
 
 module.exports = router;
